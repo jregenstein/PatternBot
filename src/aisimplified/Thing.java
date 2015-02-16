2df5a8e4f20a015282b05938a352df8e7f816b33
jregenstein <jacobregenstein@gmail.com>
2015-02-15 20:29:05 -0500

    first commit I hope this doesn't fuck things up

--- src/aisimplified/Thing.java
+++ src/aisimplified/Thing.java
@@ -9,7 +9,13 @@ package aisimplified;
 import java.util.HashSet;
 
 /**
- *
+ *Thing is usually a very general word, but in this program, Thing has a very 
+ * specific definition: a thing is a symbol or a pattern. This is because 
+ * patterns need to be able to contain both symbols and objects. I realize that
+ * at this point I could just use object class to the same effect, but I'm adding
+ * this in cause I figure at some point I'll want to have methods that both
+ * symbol and patterns use, like having a static list holding references to the
+ * patterns they're used it.
  * @author jacobregenstein
  */
 public abstract class Thing {
